f78a9f1d5f772ef30886319c75d90c4839432685
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-10 12:50:46 +0530

    nothing to commit here

---
+++ src/main/java/com/example/spring/boot/payment/gateway/SpringBootPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.payment.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/boot/payment/gateway/SpringBootPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.payment.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
